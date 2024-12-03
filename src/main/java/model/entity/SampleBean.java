757af86337fb7c3bd370b62262cd96c13c5934bb
comsize-sakagami <hisashi.sakagami@comsize.com>
2024-12-03 10:37:24 +0900

    テスト

--- src/main/java/model/entity/SampleBean.java
+++ src/main/java/model/entity/SampleBean.java
@@ -1,5 +1,20 @@
 package model.entity;
 
 public class SampleBean {
-
+	private String name ;
+	private int age;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
 }
