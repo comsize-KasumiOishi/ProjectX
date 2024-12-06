402ac4e32aceb158adf6283fe28384c6856aaffe
comsize-sakagami <hisashi.sakagami@comsize.com>
2024-12-06 15:42:15 +0900

    TaskCategoryUserStatusBeanにgetterとsetterを追加

--- src/main/java/model/entity/TaskCategoryUserStatusBean.java
+++ src/main/java/model/entity/TaskCategoryUserStatusBean.java
@@ -14,7 +14,100 @@ public class TaskCategoryUserStatusBean {
 	private String statusName;		
 	private String memo;		
 	private Timestamp createDateTime;		
-	private Timestamp updateDAtetime;		
+	private Timestamp updateDAtetime;
+	
+	public TaskCategoryUserStatusBean() {
+		
+	}
 
+	public int getTaskId() {
+		return taskId;
+	}
+
+	public void setTaskId(int taskId) {
+		this.taskId = taskId;
+	}
+
+	public String getTaskName() {
+		return taskName;
+	}
+
+	public void setTaskName(String taskName) {
+		this.taskName = taskName;
+	}
+
+	public int getCategoryId() {
+		return categoryId;
+	}
+
+	public void setCategoryId(int categoryId) {
+		this.categoryId = categoryId;
+	}
+
+	public Date getLimitDate() {
+		return limitDate;
+	}
+
+	public void setLimitDate(Date limitDate) {
+		this.limitDate = limitDate;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getStatusCode() {
+		return statusCode;
+	}
+
+	public void setStatusCode(String statusCode) {
+		this.statusCode = statusCode;
+	}
+
+	public String getStatusName() {
+		return statusName;
+	}
+
+	public void setStatusName(String statusName) {
+		this.statusName = statusName;
+	}
+
+	public String getMemo() {
+		return memo;
+	}
+
+	public void setMemo(String memo) {
+		this.memo = memo;
+	}
+
+	public Timestamp getCreateDateTime() {
+		return createDateTime;
+	}
+
+	public void setCreateDateTime(Timestamp createDateTime) {
+		this.createDateTime = createDateTime;
+	}
+
+	public Timestamp getUpdateDAtetime() {
+		return updateDAtetime;
+	}
+
+	public void setUpdateDAtetime(Timestamp updateDAtetime) {
+		this.updateDAtetime = updateDAtetime;
+	}
+
+	
 
 }
