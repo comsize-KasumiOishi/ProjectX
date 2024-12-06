2d71cad8401a925c8eca877aa762885edc726e99
comsize-sakagami <hisashi.sakagami@comsize.com>
2024-12-06 15:40:13 +0900

    modelパッケージのentityパッケージの中にTaskCategoryUserStatusBeanの作成

---
+++ src/main/java/model/entity/TaskCategoryUserStatusBean.java
@@ -0,0 +1,20 @@
+package model.entity;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+
+public class TaskCategoryUserStatusBean {
+	private   int  taskId;		
+	private String taskName;		
+	private int categoryId;		
+	private Date limitDate;		
+	private String userId;		
+	private String userName;		
+	private String statusCode;		
+	private String statusName;		
+	private String memo;		
+	private Timestamp createDateTime;		
+	private Timestamp updateDAtetime;		
+
+
+}
