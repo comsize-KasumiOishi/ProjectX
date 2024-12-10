e3cc57c0f4f9fb7cc5f1c76e8f0792c9f24ff094
comsize-shinoanju <anju.shino@comsize.com>
2024-12-10 11:44:15 +0900

    tcusbean localdate 更新

--- src/main/java/model/entity/TaskCategoryUserStatusBean.java
+++ src/main/java/model/entity/TaskCategoryUserStatusBean.java
@@ -1,14 +1,14 @@
 package model.entity;
 
-import java.sql.Date;
 import java.sql.Timestamp;
+import java.time.LocalDate;
 
 public class TaskCategoryUserStatusBean {
 	private   int  taskId;		
 	private String taskName;		
 	private int categoryId;	
 	private String categoryName;
-	private Date limitDate;		
+	private LocalDate limitDate;		
 	private String userId;		
 	private String userName;		
 	private String statusCode;		
@@ -53,11 +53,11 @@ public class TaskCategoryUserStatusBean {
 		this.categoryName = categoryName;
 	}
 
-	public Date getLimitDate() {
+	public LocalDate getLimitDate() {
 		return limitDate;
 	}
 
-	public void setLimitDate(Date limitDate) {
+	public void setLimitDate(LocalDate limitDate) {
 		this.limitDate = limitDate;
 	}
 
