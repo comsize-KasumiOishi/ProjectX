0e8c889e8430a9448a8085d277c3fa6320b3ea51
comsize-Kenji <kenji.yanagisawa@comsize.com>
2024-12-12 15:34:03 +0900

    コメント登録用SQL文

---
+++ TESTDATA_t_comment_setting.sql
@@ -0,0 +1,118 @@
+--ユーザマスタ t_comment(17件分)
+
+--task_id = 1
+
+--①task_id = 1   コメント 1/8件
+INSERT INTO 
+	t_comment (comment_id, task_id, user_id, comment, update_datetime) 
+VALUES 
+	('1','1','admin', 'あいうえお','2024-12-05 00:00:00');
+	
+--②task_id = 1   コメント 2/8件
+INSERT INTO 
+	t_comment (comment_id, task_id, user_id, comment, update_datetime)
+VALUES 
+	('2','1','p_daisuke', 'かきくけこ','2024-12-05 00:00:00');
+	
+--③task_id = 1   コメント 3/8件
+INSERT INTO 
+	t_comment (comment_id, task_id, user_id, comment, update_datetime) 
+VALUES 
+	('3','1','p_hikaru', 'さしすせそ','2024-12-05 00:00:00');
+	
+--④task_id = 1   コメント 4/8件
+INSERT INTO 
+	t_comment (comment_id, task_id, user_id, comment, update_datetime)  
+VALUES 
+	('4','1','p_koji', 'たちつてと','2024-12-05 00:00:00');
+	
+--⑤task_id = 1   コメント 5/8件
+INSERT INTO 
+	t_comment (comment_id, task_id, user_id, comment, update_datetime) 
+VALUES 
+	('5','1','p_raul', 'なにぬねの','2024-12-05 00:00:00');
+	
+--⑥task_id = 1   コメント 6/8件
+INSERT INTO 
+	t_comment (comment_id, task_id, user_id, comment, update_datetime) 
+VALUES 
+	('6','1','p_ren', 'はひふへほ','2024-12-05 00:00:00');
+	
+--⑦task_id = 1   コメント 7/8件
+INSERT INTO 
+	t_comment (comment_id, task_id, user_id, comment, update_datetime)  
+VALUES 
+	('7','1','p_ryohei', 'まみむめも','2024-12-05 00:00:00');
+	
+--⑧task_id = 1   コメント 8/8件
+INSERT INTO 
+	t_comment (comment_id, task_id, user_id, comment, update_datetime)   
+VALUES 
+	('8','1','p_ryota', 'やゆよ','2024-12-05 00:00:00');
+
+--task_id = 2
+
+--task_id = 2   コメント 1/4件
+INSERT INTO 
+	t_comment (comment_id, task_id, user_id, comment, update_datetime)   
+VALUES 
+	('9','2','p_shota', 'らりるれろ','2024-12-05 00:00:00');
+	
+--task_id = 2   コメント 2/4件
+INSERT INTO 
+	t_comment (comment_id, task_id, user_id, comment, update_datetime)   
+VALUES 
+	('10','2','p_tatsuya', 'わをん','2024-12-05 00:00:00');
+	
+
+--task_id = 2   コメント 3/4件
+INSERT INTO 
+	t_comment (comment_id, task_id, user_id, comment, update_datetime)   
+VALUES 
+	('11','2','p_raul', 'ABCD','2024-12-05 00:00:00');
+	
+--task_id = 2   コメント 4/4件
+INSERT INTO 
+	t_comment (comment_id, task_id, user_id, comment, update_datetime)   
+VALUES 
+	('12','2','p_tatsuya', 'EFG','2024-12-05 00:00:00');
+	
+--task_id = 3
+
+--task_id = 3   コメント 1/3件
+INSERT INTO 
+	t_comment (comment_id, task_id, user_id, comment, update_datetime)   
+VALUES 
+	('13','3','p_tatsuya', 'HIJ','2024-12-05 00:00:00');
+	
+--task_id = 3   コメント 2/3件
+INSERT INTO 
+	t_comment (comment_id, task_id, user_id, comment, update_datetime)   
+VALUES 
+	('14','3','p_shota', 'KLM','2024-12-05 00:00:00');
+	
+--task_id = 3   コメント 3/3件
+INSERT INTO 
+	t_comment (comment_id, task_id, user_id, comment, update_datetime)   
+VALUES 
+	('15','3','p_ryota', 'NOPQ','2024-12-05 00:00:00');
+	
+--task_id = 4
+
+--task_id = 4   コメント 1/2件
+INSERT INTO 
+	t_comment (comment_id, task_id, user_id, comment, update_datetime)   
+VALUES 
+	('16','3','p_ryohei', 'RXW','2024-12-05 00:00:00');
+	
+--task_id = 4   コメント 2/2件
+INSERT INTO 
+	t_comment (comment_id, task_id, user_id, comment, update_datetime)   
+VALUES 
+	('17','3','p_ren', 'YZ','2024-12-05 00:00:00');
+	
+--task_id = 5
+
+--task_id = 5   コメント 0/0件
+
+
