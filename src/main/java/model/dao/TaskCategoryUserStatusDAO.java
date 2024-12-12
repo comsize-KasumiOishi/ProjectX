bb818ef093500263dbd1a4b5ff26d3051fbfe72a
comsize-KasumiOishi <kasumi.oishi@comsize.com>
2024-12-12 16:41:16 +0900

    LoginServletとDAOのselectLimitDateメソッド更新to坂上さん

--- src/main/java/model/dao/TaskCategoryUserStatusDAO.java
+++ src/main/java/model/dao/TaskCategoryUserStatusDAO.java
@@ -98,10 +98,10 @@ public class TaskCategoryUserStatusDAO {
 		}
 		return statusList;
 	}
-	
+
 	public int insert(TaskCategoryUserStatusBean tcusbean) throws ClassNotFoundException, SQLException {
 		//データベースにタスク情報を登録するメソッド
-		
+
 		//データベースに接続してタスク情報を登録する
 		String sql = "INSERT INTO t_task (task_name, category_id, limit_date, user_id, status_code, memo) VALUES (?, ?, ?, ?, ?, ?)";
 
@@ -127,12 +127,12 @@ public class TaskCategoryUserStatusDAO {
 		return res;
 
 	}
-	
+
 	public int delete(int taskId) throws SQLException, ClassNotFoundException {
 		//削除した件数を返すメソッド
-		
+
 		int count = 0;
-		
+
 		//データベースに接続してタスクテーブルの情報を削除する
 		String sql = "DELETE FROM t_task WHERE task_id = ?";
 
@@ -146,149 +146,173 @@ public class TaskCategoryUserStatusDAO {
 		}
 		return count;
 	}
+
 	//タスク編集を行うメソッド
-		public int update(TaskCategoryUserStatusBean tcus) throws SQLException, ClassNotFoundException {
-//		変更したデータの数を送る変数
-			int count;
-			
-			try (Connection con = ConnectionManager.getConnection();
-					PreparedStatement pstmt = con
-							.prepareStatement(
-				"update t_task set task_name = ?, category_id = ?, limit_date = ? , user_id = ? , status_code = ? , memo = ?  where task_id = ?")) {
-//				タスク名の変更
-				pstmt.setString(1, tcus.getTaskName());
-//				カテゴリーの変更
-				pstmt.setInt(2, tcus.getCategoryId());
-//				期限の変更
-				pstmt.setDate(3, tcus.getLimitDate());
-//				担当者の変更
-				pstmt.setString(4, tcus.getUserId());
-//				ステータスの変更
-				pstmt.setString(5, tcus.getStatusCode());
-//				メモの変更
-				pstmt.setString(6, tcus.getMemo());
-//				変更するタスクID
-				pstmt.setInt(7, tcus.getTaskId());
-				
-				count = pstmt.executeUpdate();
+	public int update(TaskCategoryUserStatusBean tcus) throws SQLException, ClassNotFoundException {
+		//		変更したデータの数を送る変数
+		int count;
+
+		try (Connection con = ConnectionManager.getConnection();
+				PreparedStatement pstmt = con
+						.prepareStatement(
+								"update t_task set task_name = ?, category_id = ?, limit_date = ? , user_id = ? , status_code = ? , memo = ?  where task_id = ?")) {
+			//				タスク名の変更
+			pstmt.setString(1, tcus.getTaskName());
+			//				カテゴリーの変更
+			pstmt.setInt(2, tcus.getCategoryId());
+			//				期限の変更
+			pstmt.setDate(3, tcus.getLimitDate());
+			//				担当者の変更
+			pstmt.setString(4, tcus.getUserId());
+			//				ステータスの変更
+			pstmt.setString(5, tcus.getStatusCode());
+			//				メモの変更
+			pstmt.setString(6, tcus.getMemo());
+			//				変更するタスクID
+			pstmt.setInt(7, tcus.getTaskId());
+
+			count = pstmt.executeUpdate();
 			return count;
-			
-		}
-		}
-		
-		public TaskCategoryUserStatusBean detail (int taskid) throws SQLException, ClassNotFoundException{
-//			選択されたタスクIDのもつ情報を詰めるためのBeanの宣言
-			TaskCategoryUserStatusBean detail = new TaskCategoryUserStatusBean();
-			
-			try (Connection con = ConnectionManager.getConnection();
-					PreparedStatement pstmt = con
-							.prepareStatement(
-						"select t1.task_id, t1.task_name, t2.category_id, t2.category_name, t1.limit_date, t3.user_id, t3.user_name, t4.status_code, t4.status_name, t1.memo, t1.create_datetime, t1.update_datetime "
-						+ " from t_task t1 "
-						+ "left outer join m_category t2 on t1.category_id = t2.category_id "
-						+ "left outer join m_user t3 on t1.user_id = t3.user_id "
-						+"left outer join m_status t4 on t1.status_code = t4.status_code  where t1.task_id = ?;"
-									)){
-//				詳細画面に送るタスクを検索するために参照するタスクID
-				pstmt.setInt(1, taskid);
-				
-//				タスク検索を行う
-				ResultSet res = pstmt.executeQuery();
-				
-//				タスク詳細画面に必要な情報をBeanに詰める
-				while(res.next()) {
-//					タスクID
-					int detailTaskId = res.getInt("t1.task_id");
-					
-//					タスク名
-					String detailTaskName = res.getString("t1.task_name");
-					
-//					カテゴリーID
-					int detailCategoryId = res.getInt("t2.category_id");
-					
-//					カテゴリー名
-					String detailCategoryName = res.getString("t2.category_name");
-					
-//					期限
-					Date detailLimitDate = res.getDate("t1.limit_date");
-					
-//					ユーザーID
-					String detailUserId = res.getString("t3.user_id");
-					
-//					ユーザー名
-					String detailUserName = res.getString("t3.user_name");
-					
-//					ステータスコード
-					String detailStatusCode = res.getString("t4.status_code");
-					
-//					ステータス名
-					String detailStatusName = res.getString("t4.status_name");
-					
-//					メモ
-					String detailMemo = res.getString("t1.memo");
-					
-//					タスク作成時刻
-					Timestamp detailCreateDateTime = res.getTimestamp("t1.create_datetime");
 
-//					タスク更新時刻
-					Timestamp detailUpdateDateTime = res.getTimestamp("t1.update_datetime");
-					
-//					各項目をBeanにセットする
-					detail.setTaskId(detailTaskId);
-					detail.setTaskName(detailTaskName);
-					detail.setCategoryId(detailCategoryId);
-					detail.setCategoryName(detailCategoryName); 
-					detail.setLimitDate(detailLimitDate);
-					detail.setUserId(detailUserId);
-					detail.setUserName(detailUserName);
-					detail.setStatusCode(detailStatusCode);
-					detail.setStatusName(detailStatusName);
-					detail.setMemo(detailMemo);
-					detail.setCreateDateTime(detailCreateDateTime);
-					detail.setUpdateDateTime(detailUpdateDateTime);
-				}
-				
+		}
+	}
+
+	public TaskCategoryUserStatusBean detail(int taskid) throws SQLException, ClassNotFoundException {
+		//			選択されたタスクIDのもつ情報を詰めるためのBeanの宣言
+		TaskCategoryUserStatusBean detail = new TaskCategoryUserStatusBean();
+
+		try (Connection con = ConnectionManager.getConnection();
+				PreparedStatement pstmt = con
+						.prepareStatement(
+								"select t1.task_id, t1.task_name, t2.category_id, t2.category_name, t1.limit_date, t3.user_id, t3.user_name, t4.status_code, t4.status_name, t1.memo, t1.create_datetime, t1.update_datetime "
+										+ " from t_task t1 "
+										+ "left outer join m_category t2 on t1.category_id = t2.category_id "
+										+ "left outer join m_user t3 on t1.user_id = t3.user_id "
+										+ "left outer join m_status t4 on t1.status_code = t4.status_code  where t1.task_id = ?;")) {
+			//				詳細画面に送るタスクを検索するために参照するタスクID
+			pstmt.setInt(1, taskid);
+
+			//				タスク検索を行う
+			ResultSet res = pstmt.executeQuery();
+
+			//				タスク詳細画面に必要な情報をBeanに詰める
+			while (res.next()) {
+				//					タスクID
+				int detailTaskId = res.getInt("t1.task_id");
+
+				//					タスク名
+				String detailTaskName = res.getString("t1.task_name");
+
+				//					カテゴリーID
+				int detailCategoryId = res.getInt("t2.category_id");
+
+				//					カテゴリー名
+				String detailCategoryName = res.getString("t2.category_name");
+
+				//					期限
+				Date detailLimitDate = res.getDate("t1.limit_date");
+
+				//					ユーザーID
+				String detailUserId = res.getString("t3.user_id");
+
+				//					ユーザー名
+				String detailUserName = res.getString("t3.user_name");
+
+				//					ステータスコード
+				String detailStatusCode = res.getString("t4.status_code");
+
+				//					ステータス名
+				String detailStatusName = res.getString("t4.status_name");
+
+				//					メモ
+				String detailMemo = res.getString("t1.memo");
+
+				//					タスク作成時刻
+				Timestamp detailCreateDateTime = res.getTimestamp("t1.create_datetime");
+
+				//					タスク更新時刻
+				Timestamp detailUpdateDateTime = res.getTimestamp("t1.update_datetime");
+
+				//					各項目をBeanにセットする
+				detail.setTaskId(detailTaskId);
+				detail.setTaskName(detailTaskName);
+				detail.setCategoryId(detailCategoryId);
+				detail.setCategoryName(detailCategoryName);
+				detail.setLimitDate(detailLimitDate);
+				detail.setUserId(detailUserId);
+				detail.setUserName(detailUserName);
+				detail.setStatusCode(detailStatusCode);
+				detail.setStatusName(detailStatusName);
+				detail.setMemo(detailMemo);
+				detail.setCreateDateTime(detailCreateDateTime);
+				detail.setUpdateDateTime(detailUpdateDateTime);
 			}
-//			Beanに詰めた情報を戻り値として戻す
-			return detail;
-			
+
 		}
-		
-		public List<TaskCategoryUserStatusBean> selectAll() throws SQLException, ClassNotFoundException {
+		//			Beanに詰めた情報を戻り値として戻す
+		return detail;
 
-			List<TaskCategoryUserStatusBean> taskList = new ArrayList<TaskCategoryUserStatusBean>();
+	}
 
-			// データベースへの接続の取得、Statementの取得、SQLステートメントの実行
-			try (Connection con = ConnectionManager.getConnection();
-					PreparedStatement pstmt = con.prepareStatement(
-							"SELECT t1.task_id, t1.task_name, t2.category_name, t1.limit_date, t3.user_name, t4.status_name, t1.memo FROM t_task t1 JOIN m_category t2 ON t1.category_id = t2.category_id JOIN m_user t3 ON t1.user_id = t3.user_id JOIN m_status t4 ON t1.status_code = t4.status_code");
-					ResultSet res = pstmt.executeQuery()) {
+	public List<TaskCategoryUserStatusBean> selectAll() throws SQLException, ClassNotFoundException {
 
-				// 結果の操作
-				while (res.next()) {
-					int taskId = res.getInt("t1.task_id");
-					String taskName = res.getString("t1.task_name");
-					String categoryName = res.getString("t2.category_name");
-					Date limitDate = res.getDate("t1.limit_date");
-					String userName = res.getString("t3.user_name");
-					String statusName = res.getString("t4.status_name");
-					String memo = res.getString("t1.memo");
-					System.out.println("タスクID 一覧サーブレット:"+taskId);
-					
+		List<TaskCategoryUserStatusBean> taskList = new ArrayList<TaskCategoryUserStatusBean>();
 
-					TaskCategoryUserStatusBean taskCategoryUserStatus = new TaskCategoryUserStatusBean();
-					taskCategoryUserStatus.setTaskId(taskId);
-					taskCategoryUserStatus.setTaskName(taskName);
-					taskCategoryUserStatus.setCategoryName(categoryName);
-					taskCategoryUserStatus.setLimitDate(limitDate);
-					taskCategoryUserStatus.setUserName(userName);
-					taskCategoryUserStatus.setStatusName(statusName);
-					taskCategoryUserStatus.setMemo(memo);
+		// データベースへの接続の取得、Statementの取得、SQLステートメントの実行
+		try (Connection con = ConnectionManager.getConnection();
+				PreparedStatement pstmt = con.prepareStatement(
+						"SELECT t1.task_id, t1.task_name, t2.category_name, t1.limit_date, t3.user_name, t4.status_name, t1.memo FROM t_task t1 JOIN m_category t2 ON t1.category_id = t2.category_id JOIN m_user t3 ON t1.user_id = t3.user_id JOIN m_status t4 ON t1.status_code = t4.status_code");
+				ResultSet res = pstmt.executeQuery()) {
 
-					taskList.add(taskCategoryUserStatus);
-				}
+			// 結果の操作
+			while (res.next()) {
+				int taskId = res.getInt("t1.task_id");
+				String taskName = res.getString("t1.task_name");
+				String categoryName = res.getString("t2.category_name");
+				Date limitDate = res.getDate("t1.limit_date");
+				String userName = res.getString("t3.user_name");
+				String statusName = res.getString("t4.status_name");
+				String memo = res.getString("t1.memo");
+				System.out.println("タスクID 一覧サーブレット:" + taskId);
+
+				TaskCategoryUserStatusBean taskCategoryUserStatus = new TaskCategoryUserStatusBean();
+				taskCategoryUserStatus.setTaskId(taskId);
+				taskCategoryUserStatus.setTaskName(taskName);
+				taskCategoryUserStatus.setCategoryName(categoryName);
+				taskCategoryUserStatus.setLimitDate(limitDate);
+				taskCategoryUserStatus.setUserName(userName);
+				taskCategoryUserStatus.setStatusName(statusName);
+				taskCategoryUserStatus.setMemo(memo);
+
+				taskList.add(taskCategoryUserStatus);
 			}
-			return taskList;
-
 		}
+		return taskList;
+
+	}
+
+	public Date selectLimitDate(String userId) throws ClassNotFoundException, SQLException {
+		//カテゴリマスタの情報一覧を格納したリストを返すメソッド
+
+		//データベースに接続してカテゴリマスタの情報を取得する
+		String sql = "SELECT limit_date FROM t_task WHERE user_id = ?";
+
+		try (Connection con = ConnectionManager.getConnection();
+				PreparedStatement pstmt = con.prepareStatement(sql)) {
+
+			pstmt.setString(1, userId);
+
+			//SQLステートメントの実行(参照系)
+			ResultSet res = pstmt.executeQuery();
+
+			if (res.next()) {
+				Date date = res.getDate("limit_date");
+
+				return date;
+
+			} else {
+				return null;
+			}
+		}
+	}
 }
--- src/main/java/servlet/LoginServlet.java
+++ src/main/java/servlet/LoginServlet.java
@@ -1,7 +1,9 @@
 package servlet;
 
 import java.io.IOException;
+import java.sql.Date;
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.List;
 
 import javax.servlet.RequestDispatcher;
@@ -11,6 +13,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 import model.dao.TaskCategoryUserStatusDAO;
 import model.dao.UserDAO;
@@ -23,19 +27,20 @@ import model.entity.UserBean;
 @WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LoginServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public LoginServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
@@ -43,67 +48,100 @@ public class LoginServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 		request.setCharacterEncoding("UTF-8");
-		
+
 		//ログイン画面で入力されたユーザIDとパスワードを取得する
 		String userId = request.getParameter("userid");
 		String password = request.getParameter("password");
-		
+
 		//ユーザIDまたはパスワードが未入力だった場合ログイン失敗画面に遷移する
-		if(userId.isEmpty() || password.isEmpty()) {
+		if (userId.isEmpty() || password.isEmpty()) {
 			RequestDispatcher rd = request.getRequestDispatcher("login-failure.jsp");
 			rd.forward(request, response);
 		}
-		
+
 		//UserDAOのインスタンスを生成する
 		UserDAO userdao = new UserDAO();
-		
+
 		try {
 			//UserBean型の変数にUserDAOのログイン認証メソッドを格納する
 			UserBean userbean = userdao.logincheck(userId, password);
-			
+
 			//UserBean型の変数がnullでない場合はログイン認証に成功する
 			if (userbean != null) {
-				
+
 				//ユーザIDとパスワードとユーザー名をセッションに格納する
 				HttpSession session = request.getSession();
 				session.setAttribute("userId", userId);
 				session.setAttribute("password", password);
 				session.setAttribute("userName", userbean.getUserName());
+
+				//タスクの期限が迫っているユーザに対して、ログイン時にアラートを出す
+				//アラートは現在日時から1週間以内に設定
+				//TaskCategoryUserStatusDAOクラスのselectLimitDateメソッドを呼び出す
+				TaskCategoryUserStatusDAO tcusdao = new TaskCategoryUserStatusDAO();
+
+				//Date型のLimitDateを取り出して変数dateに詰める
+				Date date = tcusdao.selectLimitDate(userId);
+
+				//LimitDateをDate型からString型にキャストする
+				String strDate = date.toString();
 				
-				//メニュー画面に遷移する
-				RequestDispatcher rd = request.getRequestDispatcher("menu.jsp");
-				rd.forward(request, response);
-			}
-			else {
+				//String型のstrDateをLocalDate型にキャストする
+				LocalDate limitDate = LocalDate.parse(strDate);
+				
+				//タスクの期限から1週間引く
+				LocalDate alertDate = limitDate.minusDays(7);
+				System.out.println("タスクの期限から1週間引く:" + alertDate);
+
+				//タスクの期限が現在日時から1週間以内だったら警告メッセージを出す
+				if (alertDate.isBefore(limitDate)) {
+					JFrame frame = new JFrame();
+					
+					JOptionPane.showMessageDialog(null, "メッセージだよ");
+
+					JOptionPane.showMessageDialog(frame, "タスクの期限が迫っています", "警告",
+							JOptionPane.ERROR_MESSAGE);
+					
+//坂上さんへ
+//タスク期限と、タスク期限の1週間前の日付を比較するだけだとアラートになってない気がします。
+//A5のタスク期限が1個だけ迫ってる、だとアラートすり抜けます。
+//タスク期限がNullだった場合については未着手です。
+					
+				}
+					//メニュー画面に遷移する
+					RequestDispatcher rd = request.getRequestDispatcher("menu.jsp");
+					rd.forward(request, response);
+				
+			} else {
 				//ログイン認証に失敗した場合はログイン失敗画面に遷移する
 				RequestDispatcher rd = request.getRequestDispatcher("login-failure.jsp");
 				rd.forward(request, response);
 			}
-			
+
 		} catch (SQLException | ClassNotFoundException e) {
 			e.printStackTrace();
 		}
-		
-		
+
 		//DAOで作成したリストをセッションに詰める
 		TaskCategoryUserStatusDAO tcusdao = new TaskCategoryUserStatusDAO();
 		List<TaskCategoryUserStatusBean> categoryList = null;
 		List<TaskCategoryUserStatusBean> userList = null;
 		List<TaskCategoryUserStatusBean> statusList = null;
-		
+
 		try {
 			categoryList = tcusdao.selectCategoryId();
 			userList = tcusdao.selectUserId();
 			statusList = tcusdao.selectStatusCode();
-			
+
 			HttpSession session = request.getSession();
 			session.setAttribute("categoryList", categoryList);
 			session.setAttribute("userList", userList);
 			session.setAttribute("statusList", statusList);
-			
+
 		} catch (ClassNotFoundException | SQLException e) {
 			e.printStackTrace();
 		}
