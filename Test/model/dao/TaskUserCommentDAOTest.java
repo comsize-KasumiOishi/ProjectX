970bffa8c7e65bbcbb39caa5ea0be183f40aec6b
comsize-sakagami <hisashi.sakagami@comsize.com>
2024-12-17 15:27:19 +0900

    各種テストDAOのテスト

---
+++ Test/model/dao/TaskUserCommentDAOTest.java
@@ -0,0 +1,14 @@
+package model.dao;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TaskUserCommentDAOTest {
+
+	@Test
+	void test() {
+		fail("まだ実装されていません");
+	}
+
+}
