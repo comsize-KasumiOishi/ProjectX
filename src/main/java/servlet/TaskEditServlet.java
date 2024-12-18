03b84a309a2b33cab881ee438e5809c05e15199b
comsize-sakagami <hisashi.sakagami@comsize.com>
2024-12-18 11:06:42 +0900

    編集Servletの記載を一部変更しました

--- src/main/java/servlet/TaskEditServlet.java
+++ src/main/java/servlet/TaskEditServlet.java
@@ -87,11 +87,10 @@ public class TaskEditServlet extends HttpServlet {
 			//カテゴリーIDをint型に型変換
 			int updateCategoryId = Integer.parseInt(updatestrCategoryId);
 			//カテゴリーIDの妥当性チェック
-			if(!(updateCategoryId == 1 || updateCategoryId == 2)) {
+			if (!(updateCategoryId == 1 || updateCategoryId == 2)) {
 				url = "task-edit-failure.jsp";
 			}
-			
-			
+
 			//期限を取得
 			String updatestrLimitDate = request.getParameter("updatelimitdate");
 			//strLimitDate妥当性チェック
@@ -99,30 +98,32 @@ public class TaskEditServlet extends HttpServlet {
 			LocalDate limitDate = null;
 			//sql.date型の期限を格納する変数Dateを宣言
 			java.sql.Date sqlDate = null;
-			try {
-				//strLimitDateをLocalDate型にキャスト
-				limitDate = LocalDate.parse(updatestrLimitDate, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-				//今日の日付を取得してLocalDate型の変数todayに代入
-				LocalDate today = LocalDate.now();
-				//今日の日付と入力された日付を比較する
-				if(today.isAfter(limitDate)) {
-					RequestDispatcher rd = request.getRequestDispatcher("task-edit-failure.jsp");
-					rd.forward(request, response);
+			if (updatestrLimitDate == null || updatestrLimitDate.isEmpty()) {
+
+			} else {
+				try {
+					//strLimitDateをLocalDate型にキャスト
+					limitDate = LocalDate.parse(updatestrLimitDate, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+					//今日の日付を取得してLocalDate型の変数todayに代入
+					LocalDate today = LocalDate.now();
+					//今日の日付と入力された日付を比較する
+					if (today.isAfter(limitDate)) {
+						RequestDispatcher rd = request.getRequestDispatcher("task-edit-failure.jsp");
+						rd.forward(request, response);
+					}
+					//todayをString型にキャスト
+					String strToday = today.toString();
+
+					//セッションスコープに今日の日付を設定する
+					session.setAttribute("strToday", strToday);
+
+					//LocalDate型のlimitDateをsql.date型に変換する
+					sqlDate = java.sql.Date.valueOf(limitDate);
+				} catch (DateTimeParseException | NullPointerException e) {
+					url = "task-edit-failure.jsp";
 				}
-				//todayをString型にキャスト
-				String strToday = today.toString();
-				
-				//セッションスコープに今日の日付を設定する
-				session.setAttribute("strToday", strToday);
-				
-				//LocalDate型のlimitDateをsql.date型に変換する
-				sqlDate = java.sql.Date.valueOf(limitDate);
-			} catch (DateTimeParseException | NullPointerException e) {
-				url = "task-edit-failure.jsp";
 			}
-			
-			
-			
+
 			//セレクトボックスでユーザーIDを取得
 			String updateUserId = request.getParameter("updateuserid");
 			//updateUserIdの妥当性チェック
@@ -132,8 +133,7 @@ public class TaskEditServlet extends HttpServlet {
 			if (count < 0 || count > 24) {
 				url = "task-edit-failure.jsp";
 			}
-			
-			
+
 			//セレクトボックスでステータスコードを取得
 			String updateStatusCode = request.getParameter("updatestatuscode");
 			//statusCodeの文字数をcountに代入
@@ -143,28 +143,26 @@ public class TaskEditServlet extends HttpServlet {
 				url = "task-edit-failure.jsp";
 			}
 			//範囲チェック
-			if(!(updateStatusCode.equals("00") || updateStatusCode.equals("50") || updateStatusCode.equals("99"))) {
+			if (!(updateStatusCode.equals("00") || updateStatusCode.equals("50") || updateStatusCode.equals("99"))) {
 				url = "task-edit-failure.jsp";
 			}
-			
-			
+
 			//メモを取得
 			String updateMemo = request.getParameter("updatememo");
 			//memo妥当性チェック
-			//memoの文字数をcountに代入
-			count = updateMemo.length();
-			//文字数チェック
-			if (count > 100) {
-				url = "task-edit-failure.jsp";
-			}
-			//空文字チェック
-			if(updateMemo.isEmpty()) {
+			if (updateMemo == null || updateMemo.isEmpty()) {
 				updateMemo = null;
+			} else {
+				//memoの文字数をcountに代入
+				count = updateMemo.length();
+				//文字数チェック
+				if (count > 100) {
+					url = "task-edit-failure.jsp";
+				}
 			}
-			
-			
+
 			//urlに登録失敗画面のurlマッピングが代入されていたら画面遷移する
-			if(url == "task-edit-failure.jsp"){
+			if (url == "task-edit-failure.jsp") {
 				//転送先のパスを指定して転送処理用オブジェクトを取得
 				RequestDispatcher rd = request.getRequestDispatcher(url);
 
@@ -172,7 +170,6 @@ public class TaskEditServlet extends HttpServlet {
 				rd.forward(request, response);
 				return;
 			}
-
 
 			//			入力された値が元の値と全て同じの場合、編集失敗画面に遷移する
 			if (updateTaskName.equals(tcusbean.getTaskName()) &&
@@ -194,9 +191,9 @@ public class TaskEditServlet extends HttpServlet {
 				updatetcus.setTaskId(tcusbean.getTaskId());
 				//				引数にBean型を指定し、updateメソッドを呼ぶ
 				count = dao.update(updatetcus);
-				if(count == 0) {
-					url = "task-edit-failure.jsp"; 
-				}else {
+				if (count == 0) {
+					url = "task-edit-failure.jsp";
+				} else {
 					url = "task-edit-result.jsp";
 				}
 			}
@@ -205,14 +202,14 @@ public class TaskEditServlet extends HttpServlet {
 		} catch (SQLException e) {
 			url = "task-edit-failure.jsp";
 		} catch (NullPointerException e) {
+			System.out.println("ぬるぽ");
 			url = "task-edit-failure.jsp";
-		}catch(NumberFormatException e) {
+		} catch (NumberFormatException e) {
 			url = "task-edit-failure.jsp";
 		}
-			
+
 		RequestDispatcher rd = request.getRequestDispatcher(url);
-			rd.forward(request, response);
-		
+		rd.forward(request, response);
 
 	}
 
