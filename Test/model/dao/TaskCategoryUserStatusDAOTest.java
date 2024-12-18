edd667cacb250e255e613940c9aadff083f78f8c
comsize-Kenji <kenji.yanagisawa@comsize.com>
2024-12-18 17:43:11 +0900

    タスク一覧画面　単体テスト用追記

--- Test/model/dao/TaskCategoryUserStatusDAOTest.java
+++ Test/model/dao/TaskCategoryUserStatusDAOTest.java
@@ -166,5 +166,24 @@ class TaskCategoryUserStatusDAOTest {
 		//Assert
 		assertNotNull(list);
 	}
-
+	
+	@Test
+	void タスク一覧画面表示() {
+		//準備のためのインスタンス
+		//Arrange 準備
+		TaskCategoryUserStatusDAO dao = new TaskCategoryUserStatusDAO();
+		List<TaskCategoryUserStatusBean> list = new ArrayList<TaskCategoryUserStatusBean>();
+		//Act 	実行
+		try {
+			list = dao.selectAll();
+		} catch (ClassNotFoundException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+		//Assert 検証
+		assertNotNull(list);
+	}
 }
--- Test/model/dao/TaskUserCommentDAOTest.java
+++ Test/model/dao/TaskUserCommentDAOTest.java
@@ -2,13 +2,40 @@ package model.dao;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+
+import model.entity.TaskUserCommentBean;
 
 class TaskUserCommentDAOTest {
 
 	@Test
 	void test() {
 		fail("まだ実装されていません");
+	}
+	
+	@Test
+	void コメント件数取得() {
+		
+		//準備のためのインスタンス
+				//Arrange 準備
+			TaskUserCommentDAO dao = new TaskUserCommentDAO();
+			List<TaskUserCommentBean> list = new ArrayList<TaskUserCommentBean>();
+				//Act 	実行
+				try {
+					list = dao.commentCount();
+				} catch (ClassNotFoundException e) {
+					// TODO 自動生成された catch ブロック
+					e.printStackTrace();
+				} catch (SQLException e) {
+					// TODO 自動生成された catch ブロック
+					e.printStackTrace();
+				}
+				//Assert 検証
+				assertNotNull(list);
 	}
 
 }
