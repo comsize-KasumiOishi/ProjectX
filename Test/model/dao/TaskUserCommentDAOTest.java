02d503797877f263cd7048c4632dddc597d4c85c
comsize-shinoanju <anju.shino@comsize.com>
2024-12-19 14:01:54 +0900

    TaskUserCommentDAOTest コメント登録、削除機能 更新

--- Test/model/dao/TaskUserCommentDAOTest.java
+++ Test/model/dao/TaskUserCommentDAOTest.java
@@ -12,29 +12,27 @@ import model.entity.TaskUserCommentBean;
 
 class TaskUserCommentDAOTest {
 
-
-	
 	@Test
 	void コメント件数取得() {
-		
+
 		//準備のためのインスタンス
-				//Arrange 準備
-			TaskUserCommentDAO dao = new TaskUserCommentDAO();
-			List<TaskUserCommentBean> list = new ArrayList<TaskUserCommentBean>();
-				//Act 	実行
-				try {
-					list = dao.commentCount();
-				} catch (ClassNotFoundException e) {
-					// TODO 自動生成された catch ブロック
-					e.printStackTrace();
-				} catch (SQLException e) {
-					// TODO 自動生成された catch ブロック
-					e.printStackTrace();
-				}
-				//Assert 検証
-				assertNotNull(list);
+		//Arrange 準備
+		TaskUserCommentDAO dao = new TaskUserCommentDAO();
+		List<TaskUserCommentBean> list = new ArrayList<TaskUserCommentBean>();
+		//Act 	実行
+		try {
+			list = dao.commentCount();
+		} catch (ClassNotFoundException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+		//Assert 検証
+		assertNotNull(list);
 	}
-	
+
 	@Test
 	void コメントリストを取得成功() {
 
@@ -56,7 +54,7 @@ class TaskUserCommentDAOTest {
 		//Assert 検証
 		assertNotNull(list);
 	}
-	
+
 	@Test
 	void コメントリストを取得失敗() {
 
@@ -77,6 +75,224 @@ class TaskUserCommentDAOTest {
 		}
 		//Assert 検証
 		assertTrue(list.isEmpty());
+	}
+
+	/*コメント情報を登録するメソッド
+	* @auther 篠
+	* @return int型の変数count
+	* @throws ClassNotFoundException,SQLException
+	*/
+	@Test
+	void コメント情報登録成功() {
+		//Arrange
+		TaskUserCommentBean tucbean = new TaskUserCommentBean();
+		//登録件数を格納するint型の変数countを宣言
+		int count = 0;
+		tucbean.setTaskId(1);
+		tucbean.setUserId("p_daisuke");
+		tucbean.setComment("かきくけこ");
+
+		//Act
+		TaskUserCommentDAO dao = new TaskUserCommentDAO();
+		try {
+			count = dao.insert(tucbean);
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+
+		//Assert
+		assertEquals(count, 1);
+	}
+
+	/*taskid未入力チェック
+	* @auther 篠
+	* @return int型の変数count
+	* @throws ClassNotFoundException,SQLException
+	*/
+	@Test
+	void コメント情報登録失敗タスクIDなし() {
+		//Arrange
+		TaskUserCommentBean tucbean = new TaskUserCommentBean();
+		//登録件数を格納するint型の変数countを宣言
+		int count = 0;
+		//		tucbean.setTaskId(1);
+		tucbean.setUserId("p_daisuke");
+		tucbean.setComment("かきくけこ");
+
+		//Act
+		TaskUserCommentDAO dao = new TaskUserCommentDAO();
+		try {
+			count = dao.insert(tucbean);
+			fail();
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+
+		//Assert
+		assertEquals(count, 0);
+	}
+
+	/*userid未入力チェック
+	* @auther 篠
+	* @return int型の変数count
+	* @throws ClassNotFoundException,SQLException
+	*/
+	@Test
+	void コメント情報登録失敗ユーザーIDなし() {
+		//Arrange
+		TaskUserCommentBean tucbean = new TaskUserCommentBean();
+		//登録件数を格納するint型の変数countを宣言
+		int count = 0;
+		tucbean.setTaskId(1);
+		//		tucbean.setUserId("p_daisuke");
+		tucbean.setComment("かきくけこ");
+
+		//Act
+		TaskUserCommentDAO dao = new TaskUserCommentDAO();
+		try {
+			count = dao.insert(tucbean);
+			fail();
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+			//Assert
+			assertEquals(count, 0);
+		}
+
+	}
+
+	/*userid文字数チェック
+	* @auther 篠
+	* @return int型の変数count
+	* @throws ClassNotFoundException,SQLException
+	*/
+	@Test
+	void コメント情報登録失敗ユーザーID文字数超過() {
+		//Arrange
+		TaskUserCommentBean tucbean = new TaskUserCommentBean();
+		//登録件数を格納するint型の変数countを宣言
+		int count = 0;
+		tucbean.setTaskId(1);
+		tucbean.setUserId("abcdefghijklmnopqrstuvwxyz");
+		tucbean.setComment("かきくけこ");
+
+		//Act
+		TaskUserCommentDAO dao = new TaskUserCommentDAO();
+		try {
+			count = dao.insert(tucbean);
+			fail();
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+			//Assert
+			assertEquals(count, 0);
+		}
+
+	}
+
+	/*comment未入力チェック
+	* @auther 篠
+	* @return int型の変数count
+	* @throws ClassNotFoundException,SQLException
+	*/
+	@Test
+	void コメント情報登録失敗コメントなし() {
+		//Arrange
+		TaskUserCommentBean tucbean = new TaskUserCommentBean();
+		//登録件数を格納するint型の変数countを宣言
+		int count = 0;
+		tucbean.setTaskId(1);
+		tucbean.setUserId("p_daisuke");
+		//		tucbean.setComment("かきくけこ");
+
+		//Act
+		TaskUserCommentDAO dao = new TaskUserCommentDAO();
+		try {
+			count = dao.insert(tucbean);
+			fail();
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+			//Assert
+			assertEquals(count, 0);
+		}
+
+	}
+
+	/*comment文字数チェック
+	* @auther 篠
+	* @return int型の変数count
+	* @throws ClassNotFoundException,SQLException
+	*/
+	@Test
+	void コメント情報登録失敗コメント文字数超過() {
+		//Arrange
+		TaskUserCommentBean tucbean = new TaskUserCommentBean();
+		//登録件数を格納するint型の変数countを宣言
+		int count = 0;
+		tucbean.setTaskId(1);
+		tucbean.setUserId("p_daisuke");
+		tucbean.setComment("ああああああああああああああああああ"
+				+ "あああああああああああああああああああああああ"
+				+ "あああああああああああああああああああああああ"
+				+ "あああああああああああああああああああああああ"
+				+ "ああああああああああああああ");
+
+		//Act
+		TaskUserCommentDAO dao = new TaskUserCommentDAO();
+		try {
+			count = dao.insert(tucbean);
+			fail();
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+			//Assert
+			assertEquals(count, 0);
+		}
+
+	}
+
+	/*コメント情報を削除するメソッド
+	 * @auther 篠
+	 * @return int型の変数count
+	 * @throws ClassNotFoundException,SQLException
+	 */
+	@Test
+	void コメント情報削除成功() {
+		//Arrange
+		int commentId = 2;
+		//登録件数を格納するint型の変数countを宣言
+		int count = 0;
+
+		//Act
+		TaskUserCommentDAO dao = new TaskUserCommentDAO();
+		try {
+			count = dao.delete(commentId);
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+
+		//Assert
+		assertEquals(count, 1);
+	}
+
+	/*コメントを削除するのに必要なリストを取得するメソッド
+	 * @auther 篠
+	 * @return TaskUserCommentBean型の変数list
+	 * @throws ClassNotFoundException,SQLException
+	 */
+	@Test
+	void コメント削除情報取得() {
+		//Arrange
+		int commentId = 2;
+		List<TaskUserCommentBean> list = new ArrayList<TaskUserCommentBean>();
+
+		//Act
+		TaskUserCommentDAO dao = new TaskUserCommentDAO();
+		try {
+			list = dao.deleteCommentList(commentId);
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+
+		//Assert
+		assertNotNull(list);
 	}
 
 }
